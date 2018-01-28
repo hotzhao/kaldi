8ef86476965cb3e5dba86af56b13ad6fd3ecc68c
lz <hotzhao@gmail.com>
2018-01-28 23:47:18 +0800

    minor changes

--- egs/voxforge/s5/path.sh
+++ egs/voxforge/s5/path.sh
@@ -7,7 +7,7 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 # VoxForge data will be stored in:
 # export DATA_ROOT="$KALDI_ROOT/egs/voxforge/s5/voxforge"    # e.g. something like /media/secondary/voxforge
 
-export DATA_ROOT=/home/liang/work/voice/corpus/voxforge
+export DATA_ROOT=/home/liang/work/speech/corpus/voxforge
 
 if [ -z $DATA_ROOT ]; then
   echo "You need to set \"DATA_ROOT\" variable in path.sh to point to the directory to host VoxForge's data"
